d30323edf38e8091f5d9f538a8e5bb36ca5e96e4
caffeines <sadat.talks@gmail.com>
2023-05-08 13:25:55 +0600

    Change favicon

--- components/Navigation.js
+++ components/Navigation.js
@@ -2,10 +2,8 @@
 
 import Link from "next/link"
 import React from "react"
-import { useState } from 'react';
 import ThemeSwitch from "./Theme";
 import { Navbar } from "flowbite-react";
-import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 
 const Nav = () => {
