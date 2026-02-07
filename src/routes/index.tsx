f74727a20847058414716b10c5ea5d1946d2eed4
caffeines <sadat.talks@gmail.com>
2026-02-08 02:23:48 +0600

    feat: introduce comprehensive responsive styling for mobile and tablet devices and adjust displayed company logos.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -65,9 +65,9 @@ function Home() {
           <div className="companies-title">Building solutions for companies like ✨</div>
           <div className="company-grid">
             <div className="company-logo">bKash</div>
-            <div className="company-logo">Durbin Labs</div>
             <div className="company-logo">TigerIT</div>
-            <div className="company-logo">CommChat</div>
+            <div className="company-logo">Durbin Labs</div>
+            {/* <div className="company-logo">CommChat</div> */}
           </div>
         </div>
       </section>
